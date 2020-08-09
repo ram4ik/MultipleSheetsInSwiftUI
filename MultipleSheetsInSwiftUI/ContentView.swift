1fe868b430c901df8831a81b995105d437d935b6
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-09 14:32:45 +0300

    Initial Commit

---
+++ MultipleSheetsInSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MultipleSheetsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 09.08.2020.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MultipleSheetsInSwiftUI/MultipleSheetsInSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MultipleSheetsInSwiftUIApp.swift
+//  MultipleSheetsInSwiftUI
+//
+//  Created by Ramill Ibragimov on 09.08.2020.
+//
+
+import SwiftUI
+
+@main
+struct MultipleSheetsInSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
