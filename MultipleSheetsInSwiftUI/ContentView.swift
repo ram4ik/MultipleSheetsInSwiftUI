7a73d90677bcf475211f3c39c3382c96c810e053
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2020-08-09 14:47:24 +0300

    init

--- MultipleSheetsInSwiftUI/ContentView.swift
+++ MultipleSheetsInSwiftUI/ContentView.swift
@@ -7,10 +7,44 @@
 
 import SwiftUI
 
+struct PresentedSheet: Identifiable {
+    var id: Int
+    var backColor: Color { Color(red: .random(in: 0...1), green: .random(in: 0...1), blue: .random(in: 0...1))}
+}
+
 struct ContentView: View {
+    @State private var presentedSheet: PresentedSheet? = nil
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            List {
+                ForEach(0..<99) { item in
+                    Button(action: {
+                        presentedSheet = .init(id: item)
+                    }, label: {
+                        HStack {
+                            Text("Sheet")
+                            Spacer()
+                            Text("\(item)")
+                        }
+                    })
+                }
+            }.navigationTitle("Multiple Sheets")
+        }.sheet(item: $presentedSheet) { (item) in
+            ZStack {
+                item.backColor
+                    .edgesIgnoringSafeArea(.all)
+                VStack {
+                    Text("Presented Sheet number:")
+                        .font(.title)
+                    Text("\(item.id)")
+                        .font(.system(size: 50))
+                }
+                .foregroundColor(Color.white)
+                .padding()
+                .border(Color.white)
+            }
+        }
     }
 }
 
